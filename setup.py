fe5e8c2c0c4aedd0cdca65a910022dfb04067520
TheMarauder95 <67920752+TheMarauder95@users.noreply.github.com>
2020-08-05 18:03:40 -0400

    updated to 0.1.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='pymusement',
-      version='0.1.4',
+      version='0.1.5',
       description='A python package and CLI to get wait times for rides at various theme parks',
       url='https://github.com/TheMarauder95/PyMusement',
       author='Josh Shrawder',
